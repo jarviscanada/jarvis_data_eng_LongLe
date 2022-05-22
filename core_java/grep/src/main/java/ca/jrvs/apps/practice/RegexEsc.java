b86cd09b41a2a8d0ffbf9126ce8c07edd1408b63
Hoang Long Le <lonbiapro123@gmail.com>
2022-05-22 19:30:45 +0000

    [Develop] Add java regex excerside: RegexEsc Interfact and RegexEscImp class

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexEsc.java
@@ -0,0 +1,28 @@
+package ca.jrvs.apps.practice;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface RegexEsc {
+
+    /**
+     * return true if filename extension is jpg or jpeg (case insensitive)
+     * @param filename
+     * @return
+     */
+    public boolean matchJpeg(String filename);
+
+    /**
+     * return true if ip is valid
+     * @param ip
+     * @return
+     */
+    public boolean matchIp(String ip);
+
+    /**
+     * return true if line is empty (e.g empty, white space, tabs, etc..)
+     * @param line
+     * @return
+     */
+    public boolean isEmptyLine(String line);
+}
