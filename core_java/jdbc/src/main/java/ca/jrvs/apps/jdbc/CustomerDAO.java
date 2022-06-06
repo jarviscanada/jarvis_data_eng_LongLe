7a82954bdf4175118ac007fb3bb5029bf8475141
Hoang Long Le <lonbiapro123@gmail.com>
2022-06-06 21:29:09 +0000

    [Develop] Complete transactions learning

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -59,6 +59,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
   @Override
   public Customer update(Customer dto) {
     Customer customer = null;
+    try {
+      this.connection.setAutoCommit(false);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
     try (PreparedStatement statement = this.connection.prepareStatement(UPDATE);) {
       statement.setString(1, dto.getFirstName());
       statement.setString(2, dto.getLastName());
@@ -70,8 +76,15 @@ public class CustomerDAO extends DataAccessObject<Customer> {
       statement.setString(8, dto.getZipcode());
       statement.setLong(9, dto.getId());
       statement.execute();
+      this.connection.commit();
       customer = this.findByID(dto.getId());
     } catch (SQLException e) {
+      try {
+        this.connection.rollback();
+      } catch (SQLException sqle) {
+        e.printStackTrace();
+        throw new RuntimeException(sqle);
+      }
       e.printStackTrace();
       throw new RuntimeException(e);
     }
